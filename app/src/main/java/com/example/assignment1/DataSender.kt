ead0a40fc6dd8c4f7f4bf314f6177bec7c166746
wesleychiau <wesleychiau@gmail.com>
2023-01-29 21:42:18 +0800

    Converted to okhttp library, added deviceid & sendfile functins

--- app/src/main/java/com/example/assignment1/DataSender.kt
+++ app/src/main/java/com/example/assignment1/DataSender.kt
@@ -1,32 +1,68 @@
 package com.example.assignment1
 
+import android.annotation.SuppressLint
+import android.content.ContentResolver
+import android.content.Context
+import android.os.Build
+import android.provider.Settings
+import android.telephony.TelephonyManager
 import android.util.Log
+import androidx.core.content.ContextCompat.getSystemService
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.asRequestBody
+import java.io.File
+import java.io.IOException
 
-// For HTTP sending data
-import com.android.volley.RequestQueue
-import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
-import java.util.*
 
 class DataSender() {
-    fun sendData(name: String, email: String, queue: RequestQueue) {
+    private val client = OkHttpClient()
+
+    fun getIMEI(contentResolver : ContentResolver): String {
+        val androidId: String = Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)
+        Log.d("id",androidId)
+        return androidId
+    }
+
+    fun sendFile(filepath: String) {
+        val url = "http://192.168.1.24/dashboard/receivefile.php"
+        val file = File(filepath)
+        val fileBody = file.asRequestBody("application/octet-stream".toMediaTypeOrNull())
+
+        val requestBody = MultipartBody.Builder()
+            .setType(MultipartBody.FORM)
+            .addFormDataPart("file", file.name, fileBody)
+            .build()
+
+        val request = Request.Builder()
+            .url(url)
+            .post(requestBody)
+            .build()
+
+        handleRequest((request))
+    }
+
+    fun sendData(name: String, email: String) {
         val url = "http://192.168.1.24/dashboard/dbconfig.php"
-        val postRequest: StringRequest = object : StringRequest(
-            Method.POST, url,
-            Response.Listener { response -> // response
-                Log.d("Response", response!!)
-            },
-            Response.ErrorListener { error -> // error
-                Log.d("Error.Response", error.toString())
+        val formBody = FormBody.Builder()
+            .add("name", name)
+            .add("email", email)
+            .build()
+        val request = Request.Builder().url(url).post(formBody).build()
+        handleRequest((request))
+    }
+
+    private fun handleRequest(request: Request) {
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                // Handle the error here by printing it
+                println("Error: ${e.message}")
             }
-        ) {
-            override fun getParams(): Map<String, String>? {
-                val params: MutableMap<String, String> = HashMap()
-                params["name"] = name
-                params["email"] = email
-                return params
+
+            override fun onResponse(call: Call, response: Response) {
+                // Handle the response here
+                println(response.body?.string())
             }
-        }
-        queue.add(postRequest)
+        })
     }
 }
--- app/src/main/java/com/example/assignment1/MainActivity.kt
+++ app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -3,24 +3,25 @@ package com.example.assignment1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract.Data
 import android.view.View
 import android.widget.TextView
-import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Create volley queue
+        // Initialise dataSender class
         var dataSender = DataSender();
-        val queue = Volley.newRequestQueue(this)
 
         setContentView(R.layout.activity_main)
-        findViewById<TextView>(R.id.mengrong).setOnClickListener(::clickName)
-        findViewById<TextView>(R.id.wesley).setOnClickListener(::clickName)
+        findViewById<TextView>(R.id.mengrong).setOnClickListener{
+            dataSender.getIMEI(this.contentResolver)
+        }
+        findViewById<TextView>(R.id.wesley).setOnClickListener{
+            dataSender.sendFile("/storage/emulated/0/Downloads/test.txt")
+        }
         findViewById<TextView>(R.id.jon).setOnClickListener{
-            dataSender.sendData("tesdssdst12", "test2", queue)
+            dataSender.sendData("tesdssdst12", "test2")
         }
         findViewById<TextView>(R.id.keefe).setOnClickListener(::clickName)
         findViewById<TextView>(R.id.minyao).setOnClickListener(::clickName)

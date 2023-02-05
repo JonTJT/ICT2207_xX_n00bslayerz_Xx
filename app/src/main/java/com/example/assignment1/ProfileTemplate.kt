05abbceacb1b4892be23faa06037141dbd959add
Vynorimas <jontanjunting@gmail.com>
2023-02-05 16:29:28 +0800

    Added location permissions and google maps redirection functionality

--- app/src/main/java/com/example/assignment1/ProfileTemplate.kt
+++ app/src/main/java/com/example/assignment1/ProfileTemplate.kt
@@ -1,6 +1,9 @@
 package com.example.assignment1
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
@@ -27,6 +30,7 @@ class ProfileTemplate : AppCompatActivity() {
 
         findViewById<Button>(R.id.contactUs).setOnClickListener(::contactUs)
         findViewById<TextView>(R.id.ResumeBtn).setOnClickListener(::chooseFile)
+        findViewById<Button>(R.id.location).setOnClickListener(::location)
     }
 
     private fun renderInfo(id: Int) {
@@ -44,7 +48,7 @@ class ProfileTemplate : AppCompatActivity() {
     private fun contactUs(view: View) {
         if (checkSMSPerms()) {
             writeSms("hello")
-            toast("Success")
+//            toast("Success")
         } else {
             requestSMSPerms()
         }
@@ -60,6 +64,33 @@ class ProfileTemplate : AppCompatActivity() {
 //        }
     }
 
+    private fun location(view: View) {
+        val PERMISSIONS = arrayOf(
+            android.Manifest.permission.ACCESS_COARSE_LOCATION,
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+        )
+        if (checkLocPerms(this, *PERMISSIONS)) {
+            // Check background location perms
+            if (checkLocPerms(this, android.Manifest.permission.ACCESS_BACKGROUND_LOCATION)) {
+                // send to google maps
+                redirectLocation()
+            } else {
+                val bgAlert = AlertDialog.Builder(this)
+                bgAlert.setTitle("Permission Needed!")
+                bgAlert.setMessage("Background Location Permission Needed!, tap \"Allow all the time in the next screen\"")
+                bgAlert.setPositiveButton("Yes") {
+                    dialog, which -> requestLocPerms(this, arrayOf(android.Manifest.permission.ACCESS_BACKGROUND_LOCATION), 103)
+                }
+                bgAlert.setNegativeButton("No") {
+                    dialog, which -> dialog.cancel()
+                }
+                bgAlert.show()
+            }
+        } else {
+            requestLocPerms(this, PERMISSIONS, 102)
+        }
+    }
+    // TODO: Merge all of the check and request permission into a permission handler function - Jon
     private fun checkFileStoragePerms(): Boolean{
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
         {
@@ -102,6 +133,33 @@ class ProfileTemplate : AppCompatActivity() {
     private fun requestSMSPerms() {
         println("request Perms")
         ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_SMS), 101)
+    }
+
+    private fun checkLocPerms(context: Context,vararg permissions: String): Boolean = permissions.all {
+        val bgLoc = ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+        val coarseLoc = ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+        val fineLoc = ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION)
+        val boolie1 = bgLoc == PackageManager.PERMISSION_GRANTED
+        val boolie2 = coarseLoc == PackageManager.PERMISSION_GRANTED
+        val boolie3 = fineLoc == PackageManager.PERMISSION_GRANTED
+        println("bgLoc = $boolie1, coarseLoc = $boolie2, fineLoc = $boolie3")
+        val isPerm = ContextCompat.checkSelfPermission(context,it) == PackageManager.PERMISSION_GRANTED
+        println("Checking $it: $isPerm")
+        ContextCompat.checkSelfPermission(context,it) == PackageManager.PERMISSION_GRANTED
+//        return bgLoc == PackageManager.PERMISSION_GRANTED && coarseLoc == PackageManager.PERMISSION_GRANTED && fineLoc == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestLocPerms(activity: Activity, permissions: Array<String>, requestCode: Int) {
+        println("Request location permissions: $permissions")
+        ActivityCompat.requestPermissions(activity, permissions, requestCode)
+    }
+
+    private fun redirectLocation() {
+        println("Redirect to google maps")
+        val gmmIntentUri = Uri.parse("google.navigation:q=Singapore Institute of Technology @ NYP")
+        val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+        mapIntent.setPackage("com.google.android.apps.maps")
+        startActivity(mapIntent)
     }
 
     private fun writeSms(message: String) {
